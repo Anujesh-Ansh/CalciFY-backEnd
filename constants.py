bc0870e501a149469fd3929876de6ef22345e575
Anujesh Ansh <anshanujesh@gmail.com>
2024-10-04 13:02:17 +0530

    updates

--- constants.py
+++ constants.py
@@ -1,8 +1,10 @@
+# constants.py
 from dotenv import load_dotenv
 import os
+
 load_dotenv()
 
 SERVER_URL = 'localhost'
-PORT = '8900'
-ENV = 'dev'
-GEMINI_API_KEY=os.getenv('GEMINI_API_KEY')
+PORT = int(os.getenv('PORT', 8900))  # Use environment variable or fallback to 8900
+ENV = os.getenv('ENV', 'dev')  # Use environment variable or fallback to 'dev'
+GEMINI_API_KEY = os.getenv('GEMINI_API_KEY')
